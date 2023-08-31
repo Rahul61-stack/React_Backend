1eab3add92d7d035c56403abf70c58d538ed9973
kaustubh sharma <kaustubh.sharma@makkajai.com>
2023-08-31 20:01:25 +0530

    overall refactor to meet standard flask practices and reorganised folder structure to support blueprints

---
+++ Relish/api/__init__.py
@@ -0,0 +1,51 @@
+"""
+Flask imports
+"""
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+
+"""
+Utility Libraries
+"""
+import configparser
+import json
+from datetime import datetime
+import time
+from bson import json_util
+from pymongo import MongoClient
+from utilities import *
+
+
+"""
+Config Setup
+"""
+config = configparser.ConfigParser()
+config.read("config.ini")
+dbConfig = config["DB"]
+
+client = MongoClient(
+    generateMongoConnectionString(
+        dbConfig["username"],
+        dbConfig["password"],
+        dbConfig["url"],
+        dbConfig["db"],
+    )
+)
+db = client[dbConfig["db"]]
+
+
+def init_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    @app.route("/serverrunningstatus", methods=["GET"])
+    def checkWorkingStatus():
+        startTime = time.time()
+        return {
+            "code": 200,
+            "status": "online",
+            "ping": time.time() - startTime,
+        }
+
+    return app
--- Relish/app.py
+++ Relish/app.py
@@ -1,25 +1,23 @@
-from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
-from flask_cors import CORS
-from bson import json_util
+from api import init_app
+from waitress import serve
 
-app = Flask(__name__)
-app.config['MONGO_URI'] =  'mongodb+srv://rahulsrivastav400:y0ApyM5aRyYXANwE@clusterx7.kw8zg9h.mongodb.net/Relish'
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['CORS_HEADERS'] = 'Content-Type'
-localhost = 'http://localhost:3000'
-cors = CORS(app, resources={r'/add/customer': {"origins": localhost},r'/get/customers':{"origins": localhost}})
-mongo = PyMongo(app)
-@app.route('/add/customer', methods=['POST'])
-def add_customer():
-    data = request.json
-    print(data)
-    mongo.db.Customers.insert_one(data)
-    return 'Data Added Successfully'
-@app.route('/get/customers', methods=['GET'])
-def read_records():
-    records = list(mongo.db.Customers.find())
-    print(records)
-    return jsonify(json_util.dumps(records))
+app = init_app()
+
+host = "0.0.0.0"
+port = 5001
+debug = False
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    try:
+        print("Starting Server")
+        print("PORT: ", port)
+        print("DEBUG: ", debug)
+
+        serve(
+            app,
+            host=host,
+            port=port,
+        )
+
+    except Exception as e:
+        print("INITIALIZING SERVER FAILED: ", e)
